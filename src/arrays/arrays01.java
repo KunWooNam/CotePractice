fd0095d573b8f693930c4118f8033915a667408a
kw <gwnam2077@gmail.com>
2025-02-02 22:49:10 +0900

    arrays q_01

--- src/arrays/arrays01.java
+++ src/arrays/arrays01.java
@@ -1,6 +1,18 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class arrays01 {
 
-    //
+    public static void main(String[] args) {
+        //Arrays sort
+        // input range : -100,000 ~ 100,000
+
+        int [] arr = {20, -1, 5, 7, 10, 4};
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+    }
 }
