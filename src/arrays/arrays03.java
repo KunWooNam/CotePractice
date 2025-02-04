21cd28baebe8b49ab743b87622db241b4c128538
kw <gwnam2077@gmail.com>
2025-02-04 22:21:56 +0900

    03:두개뽑아서더하기 04:모의고사찍기 1위

---
+++ src/arrays/arrays03.java
@@ -0,0 +1,33 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class arrays03 {
+
+    public static void main(String[] args) {
+        //two random number sum arrays
+        //array size : above 2 and below 100, range above 0 and below 100
+        //time complexcity : O(N^2log(N2)))
+
+        int[] arr = {2, 1, 3, 4, 1};
+        int[] result = solution(arr);
+        System.out.println(Arrays.toString(result));
+    }
+
+    //
+    private static int[] solution(int[] arr){
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                Integer sum = arr[i] * arr[j];
+                System.out.println(i + ", j:" + j);
+                System.out.println("sum = " + sum);
+                set.add(sum);
+            }
+        }
+        //Stream.sorted() 메서드는 O(k log k)의 시간복잡도
+        return set.stream().sorted().mapToInt(Integer::intValue).toArray(); //sorted()메서드는 N2logN2의 시간복잡도를 가짐
+    }
+}
---
+++ src/arrays/arrays04.java
@@ -0,0 +1,38 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class arrays04 {
+
+    public static void main(String[] args) {
+
+        int[] answer = {2,2,2,2,3,3,3,3,3,3};
+        System.out.println(Arrays.toString(solution(answer)));
+    }
+
+    private static int[] solution(int[] answer){
+        int[][] pattern = {
+                {1, 2, 3, 4, 5},
+                {2, 1, 2, 3, 2, 4, 2, 5},
+                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
+        int[] highScorer = new int[pattern.length];
+        for (int i = 0; i < answer.length ; i++) {
+            for (int j = 0; j < pattern.length; j++) {
+                if(answer[i] == pattern[j][i % pattern[j].length]{
+                        highScorer[j]++;
+                }
+            }
+
+        }
+        int maxScore = Arrays.stream(highScorer).max().getAsInt();
+        ArrayList<Integer> answers = new ArrayList<>();
+        for (int i : highScorer) {
+            if(maxScore == i){
+                answers.add(i);
+            }
+        }
+        return answers.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
